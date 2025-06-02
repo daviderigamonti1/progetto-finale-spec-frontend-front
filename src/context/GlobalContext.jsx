0dfc46176eacb90b0f207c715f2a8034cd70f8c8
daviderigamonti1 <davide.rigamonti.01@gmail.com>
2025-06-02 17:57:22 +0200

    implemented localStorage persistence for favorites

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -3,18 +3,25 @@ import { createContext, useState } from "react";
 import useDevices from "../hooks/useDevices";
 
 export const GlobalContext = createContext();
+const saved = localStorage.getItem("favorites");
 
 export function GlobalProvider({ children }) {
     const { devices } = useDevices();
-    const [favorites, setFavorites] = useState([]);
+    const [favorites, setFavorites] = useState(saved ? JSON.parse(localStorage.getItem("favorites")) : []);
 
     const isFavorite = (device) => favorites.some(fav => fav.id === device.id);
+
     function toggleFavorites(device) {
+        let updatedFavorites;
+
         if (isFavorite(device)) {
-            setFavorites(prev => prev.filter(fav => fav.id !== device.id));
+            updatedFavorites = [...favorites].filter(fav => fav.id !== device.id);
         } else {
-            setFavorites(prev => [...prev, device]);
+            updatedFavorites = [...favorites, device];
         }
+
+        setFavorites(updatedFavorites);
+        localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
     }
 
     return (
